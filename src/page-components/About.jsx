b04ee5c8da488d333ab426c7f95527e5a1c3611a
Dylan Panepucci <dylan.panepucci@gmail.com>
2025-06-15 18:15:36 -0400

    Updated INFO page to be shorter and more to the point

--- src/page-components/About.jsx
+++ src/page-components/About.jsx
@@ -5,11 +5,7 @@ export default function About() {
       <div>
         <h1 className="h1">Our Mission</h1>
         <p className="main-para">
-          Panepucci Repair and Services is a small, dedicated team specializing in field repairs for heavy equipment used in construction, mining or oil & gas. As a family owned business, we offer a level of responsiveness, attention to detail, and accountability that larger companies can’t match.
-
-With years of experience turning wrenches and solving tough problems on-site, we know how important it is to keep your equipment moving. Whether it's an unexpected breakdown or routine maintenance, we show up ready to get your machines back to work—quickly, safely, and reliably.
-
-At Panepucci Repair and Services, you’re not just another ticket in the system. You’re working directly with the people who do the work—and take pride in doing it right.
+          Panepucci Repair and Services is a small, family-owned business specializing in field repairs for heavy equipment in construction, mining, and oil & gas. We offer the responsiveness, attention to detail, and accountability larger companies can’t match. With hands-on experience solving tough problems on-site, we understand the urgency of keeping your equipment moving. Whether it’s a breakdown or routine maintenance, we show up ready to get the job done—quickly, safely, and reliably. When you work with us, you’re dealing directly with the people who take pride in doing it right.
         </p>
       </div>
     );
